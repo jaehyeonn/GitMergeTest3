5c7119b3298d99b7ed5258e54560ed1072b0bf31
ZEZE\a0104 <a01049469494@gmail.com>
2023-07-03 16:23:59 +0900

    feat : Add New

--- GitMergeTest3/Program.cs
+++ GitMergeTest3/Program.cs
@@ -4,33 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GitMergeTest3
+namespace MergeTest
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Ruler ruler = new Ruler(10);
+            // {    2023 07 03 Add user input / Beta
+            string userInput = string.Empty;
+            Console.WriteLine("This program converc Cm to Inch");
+            Console.WriteLine("Input Cm value : ");
+            userInput = Console.ReadLine();
+
+            int cmInput = 0;
+            int.TryParse(userInput, out cmInput);
+
+            Ruler ruler = new Ruler(cmInput);
             ruler.Run();
+            // }    2023 07 03 Add user input / Beta
         }
     }
 
     public class Ruler
     {
-        private const float ONE_INCH = 2.54F;
-        public int Centimeter { get; set; } = 0;
+        private const float ONE_INCH = 2.54f;
+        private int Centimeter { get; set; } = 0;
 
         public float Inch
         {
-            get { return Centimeter * ONE_INCH; }
+            get
+            {
+                return Centimeter * ONE_INCH;
+            }
             private set { Centimeter = (int)(value / ONE_INCH); }
         }
+
         public Ruler(int cmValue) { Centimeter = cmValue; }
 
         public void Run()
         {
-            Console.WriteLine($"{this.Centimeter}cm는 {this.Inch}inch 입니다.");
+            Console.WriteLine($"{this.Centimeter}cm 는 {this.Inch}inch 입니다.");
         }
     }
 }
-
